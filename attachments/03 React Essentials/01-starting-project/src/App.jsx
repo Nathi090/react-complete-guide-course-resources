47736065f650c054b36905abb8ea2bb0c9d3fa73
Natalia Solano <51279903+Nathi090@users.noreply.github.com>
2024-12-18 09:26:54 -0600

    adc

---
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -0,0 +1,50 @@
+import {useState} from 'react'
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header.jsx'
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+import { useState } from 'react';
+
+
+
+function App() {
+  useState() //only called at the beggining of the top level of the component function
+
+  function handleSelect(selectedButton) {
+    // selectedButton => dynamic to each name
+    tabContent = selectedButton
+}
+
+  return (
+    <div>  
+      <Header />    
+      <main>
+        <section id = "core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+          <CoreConcept {...CORE_CONCEPTS[0]}
+          />
+          <CoreConcept {...CORE_CONCEPTS[1]}
+          />
+          <CoreConcept {...CORE_CONCEPTS[2]}
+          />
+          <CoreConcept {...CORE_CONCEPTS[3]}
+          />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('componentes')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
